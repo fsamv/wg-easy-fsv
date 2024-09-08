2333bff5697db5356f4a01bac78ec6287e466f49
Semen MBPRO16 <sfedkin@gmail.com>
2024-09-08 19:10:58 +0300

    Add nginx https

---
+++ run-once.sh
@@ -0,0 +1,6 @@
+docker-compose up -d
+docker exec -it nginx /bin/sh
+cp /etc/nginx/servers/wg-easy.conf /etc/nginx/conf.d/.
+certbot --nginx --non-interactive --agree-tos -m webmaster@google.com -d 1460469-cz52311.tw1.ru
+nginx -s reload
+exit
