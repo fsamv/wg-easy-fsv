f752b3b02d1a869e7570ef22c883132afd96b76f
Semen MBPRO16 <sfedkin@gmail.com>
2024-10-02 14:15:58 +0300

    rm nginx conf

--- run-once.sh
+++ run-once.sh
@@ -2,5 +2,6 @@ docker compose --file docker-compose-nginx.yml up -d
 docker exec -it nginx /bin/sh
 cp /etc/nginx/servers/wg-easy.conf /etc/nginx/conf.d/.
 certbot --nginx --non-interactive --agree-tos -m webmaster@google.com -d 1460469-cz52311.tw1.ru
+rm -rf /etc/nginx/conf.d/default.conf
 nginx -s reload
 exit
