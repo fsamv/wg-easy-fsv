738f8c6c9226e41f568cd57074da3d076e5a39fe
Semen MBPRO16 <sfedkin@gmail.com>
2024-10-13 21:44:38 +0300

    Mv stunnel to 44300 port

--- run-once.sh
+++ run-once.sh
@@ -1,5 +1,5 @@
-docker compose --file docker-compose-nginx.yml up -d
-docker exec -it nginx /bin/sh
+sudo docker compose --file docker-compose-nginx-stunnel.yml up -d
+sudo docker exec -it nginx /bin/sh
 cp /etc/nginx/servers/wg-easy.conf /etc/nginx/conf.d/.
 certbot --nginx --non-interactive --agree-tos -m webmaster@google.com -d 1460469-cz52311.tw1.ru
 rm -rf /etc/nginx/conf.d/default.conf
